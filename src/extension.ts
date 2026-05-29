e6545d2aa34854c03107d2e3c24614451a72cbbd
Lucas Arantes <lucas.ruby.dev@gmail.com>
2026-05-28 23:20:34 -0400

    Add Polar sandbox config for development mode

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,26 @@ import { LicenseManager } from "@lucasprag/vscode-license";
 import { FolderProjectsProvider, getProjects } from "./projects";
 import { getRoots } from "./util";
 
+const SANDBOX = {
+  organizationId: "94b4a580-a66a-458c-9cfa-48c8d019f7e5",
+  benefitId: "bdf7934a-a72f-4b7e-ad85-03299b555821",
+  checkoutUrl: "https://sandbox-api.polar.sh/v1/checkout-links/polar_cl_jlnAI6qH8pQJxpjlGMHzL4NdazvqzFIY66fht4ZAao1/redirect",
+};
+
+const LIVE = {
+  organizationId: "d2232643-dd19-4377-84a8-3c671011baa4",
+  benefitId: "f481d7d7-1518-4572-ae0a-21a576c7fbd0",
+  checkoutUrl: "https://buy.polar.sh/polar_cl_ibAYkhoQLRrkGwfysneBiYH0Cl3xceKB5olQv1skJjc",
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  const isDev = context.extensionMode === vscode.ExtensionMode.Development;
+  const polar = isDev ? SANDBOX : LIVE;
+
   const license = new LicenseManager(context, {
-    organizationId: "d2232643-dd19-4377-84a8-3c671011baa4",
-    benefitId: "f481d7d7-1518-4572-ae0a-21a576c7fbd0",
+    ...polar,
+    sandbox: isDev,
     extensionName: "Folder Projects",
-    checkoutUrl: "https://buy.polar.sh/polar_cl_ibAYkhoQLRrkGwfysneBiYH0Cl3xceKB5olQv1skJjc",
     commandPrefix: "folderProjects.license",
     gracePeriodDays: 7,
     reminderIntervalDays: 3,
